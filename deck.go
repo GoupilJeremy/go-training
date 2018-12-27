42f5112430ed72751f2cd6c28514e6889243e939
Jérémy Goupil <goupiljeremy@gmail.com>
2018-12-27 10:39:08 +0100

    add deck.go

--- deck.go
+++ deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+)
 
 // Create a new type of `deck`
 // which is a slice of strings
@@ -23,5 +29,37 @@ func newDeck() deck {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+// return 2 values of types deck !!!!
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 func main() {
 	cards := newDeck()
-
+	cards.shuffle()
 	cards.print()
+
 }
